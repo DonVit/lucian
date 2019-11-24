66d71c9243101549db359b6c9445072c2658f7b8
Vitalie Doni <vitalie.doni@gmail.com>
2019-11-24 20:42:52 +0200

    lesson 1

---
+++ lesson1.js
@@ -0,0 +1,27 @@
+const g=567
+const k=433
+const i=g+k
+console.log('i=',i)
+
+const u=234
+const f=123
+const q=u-f
+console.log('q=',q)
+
+const a=123457689
+const b=12345
+const c=a*b
+console.log('c=',c)
+
+var v=91 
+var n=9
+var m=v/n
+console.log("m=",m)
+
+const x=7
+const y=9
+const z=Math.pow(x,y)
+console.log('z=',z)
+
+const suma=i+q+c+m+z
+console.log('i+q+c+m+z=',suma)
